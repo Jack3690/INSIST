374a45a8d5f5ceda1adb21956c1f2f391a1cada4
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2023-01-23 17:05:15 +0530

    Doc update

--- docs/conf.py
+++ docs/conf.py
@@ -2,39 +2,27 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath("../../src"))
-package_path = os.path.abspath('../../src')
-os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'PISTA'
-copyright = '2023, Avinash CK and INSIST Science team at IIA, Bangalore'
-author = 'Avinash CK and Science team at IIA, Bangalore'
+copyright = '2023, Avinash CK and INSIST science team at IIA, Bangalore'
+author = 'Avinash CK and INSIST science team at IIA, Bangalore'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.coverage',
-    'sphinx.ext.autosectionlabel',
-    'jupyter_sphinx',
-]
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. PISTA documentation master file, created by
-   sphinx-quickstart on Mon Jan 23 16:52:31 2023.
+   sphinx-quickstart on Mon Jan 23 17:04:27 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 

140c456a20ae0b131df3eeb2c3092a9f3ec01844
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2023-01-23 20:40:52 +0530

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,20 +2,17 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
-
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'PISTA'
-copyright = '2023, Avinash CK and INSIST science team at IIA, Bangalore'
-author = 'Avinash CK and INSIST science team at IIA, Bangalore'
-release = '1.0.0'
+copyright = '2023, Avinash CK and INSIST Science team at IIA, Bangalore'
+author = 'Avinash CK and INSIST Science team at IIA, Bangalore'
+release = '1.0.15'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,11 +22,8 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon'
 ]
-
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,9 +1,9 @@
 .. PISTA documentation master file, created by
-   sphinx-quickstart on Mon Jan 23 17:04:27 2023.
+   sphinx-quickstart on Mon Jan 23 20:34:21 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Python Image Simulation and Testing Application
+Welcome to PISTA's documentation!
 =================================
 
 .. toctree::

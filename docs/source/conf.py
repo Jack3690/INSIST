e244adb5b8a339e181c3dbf63b32f9741bf191ce
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2023-04-12 00:19:11 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,10 +35,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 
-html_theme = 'furo'
+#html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

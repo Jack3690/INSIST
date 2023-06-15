96be2bbe3bd40c5ac6f303fee685706c6302084e
Jack3690 <avinashck90@gmail.com>
2023-06-15 19:20:38 +0530

    Stable version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='insist-pista',
-        version='1.0.3',
+        version='1.1.0',
         description="Python Image Simulation and Testing Application",
         url='https://github.com/Jack3690/INSIST',
         author='Avinash CK',
--- src/pista/__init__.py
+++ src/pista/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.3"
+__version__ = "1.1.0"
 __author__ = 'Avinash CK'
 __credits__ = 'Indian Institute of Astrophysics'
 

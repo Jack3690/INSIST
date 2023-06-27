bbabc20b552d063594802c6d20ea36979bc4ceba
Jack3690 <avinashck90@gmail.com>
2023-06-27 22:59:43 +0530

    v2.0.0

--- src/pista/__init__.py
+++ src/pista/__init__.py
@@ -2,7 +2,7 @@ __version__ = "2.0.0"
 __author__ = 'Avinash CK'
 __credits__ = 'Indian Institute of Astrophysics'
 
-from .simulation import Imager, Mosaic, Galaxy
+from .simulation import Imager, Mosaic
 from .analysis import Analyzer, SpecAnalyzer
 from pathlib import Path
 
